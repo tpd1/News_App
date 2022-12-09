703132112bc279be2502709182bd2a722464a6d5
tpd1 <94313370+tpd1@users.noreply.github.com>
2022-12-09 19:28:10 +0000

    Topic select formatting

--- app/src/main/java/com/example/newsapp/NewsFragment.kt
+++ app/src/main/java/com/example/newsapp/NewsFragment.kt
@@ -13,6 +13,6 @@ class NewsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
+        return inflater.inflate(R.layout.fragment_news_feed, container, false)
     }
 }
